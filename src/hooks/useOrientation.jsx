b80750056b3acf05fba87c82c948e0501a7aab1f
Luis Defago <defagoluis5@gmail.com>
2023-07-20 18:35:25 -0300

    oseOrientation

---
+++ src/hooks/useOrientation.jsx
@@ -0,0 +1,30 @@
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect, useState } from 'react';
+
+export default function useOrientation() {
+    const [screenOrientation, setScreenOrientation] = useState(
+        ScreenOrientation.Orientation.PORTRAIT_DOWN
+    );
+
+    useEffect(() => {
+        const orientationChange = (currentOrientation) => {
+            const orientationValue = currentOrientation.orientationInfo.orientation;
+            setScreenOrientation(orientationValue);
+        };
+
+        const initScreenOrientation = async () => {
+            const currentOrientation = await ScreenOrientation.getOrientationAsync;
+            setScreenOrientation(currentOrientation);
+        };
+
+        const screenOrientationListener = ScreenOrientation.addOrientationChangeListener();
+
+        initScreenOrientation();
+
+        return () => {
+            ScreenOrientation.removeOrientationChangeListener(screenOrientationListener);
+        };
+    }, []);
+
+    return screenOrientation === 3 || screenOrientation === 4 ? 'LANDSCAPE' : 'PORTRAIT';
+}
