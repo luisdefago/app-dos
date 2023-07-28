c9a9db57c4f4763b3bc9fd12a1eb8b101bc6a603
Luis Defago <defagoluis5@gmail.com>
2023-07-28 18:54:05 -0300

    Styles del tab

--- src/navigation/tab.jsx
+++ src/navigation/tab.jsx
@@ -2,15 +2,82 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ShopNavigator from './shop';
 import CartNavigator from './cart';
 import OrdersNavigator from './orders';
+import { COLORS } from '../themes';
+import { Ionicons } from '@expo/vector-icons';
 
 const BottomTab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <BottomTab.Navigator initialRouteName="ShopTab" screenOptions={{ headerShown: false }}>
-            <BottomTab.Screen name="ShopTab" component={ShopNavigator} />
-            <BottomTab.Screen name="CartTab" component={CartNavigator} />
-            <BottomTab.Screen name="OrdersTab" component={OrdersNavigator} />
+        <BottomTab.Navigator
+            initialRouteName="ShopTab"
+            screenOptions={{
+                headerShown: false,
+                tabBarLabelStyle: {
+                    fontFamily: 'Inter-Bold',
+                    fontSize: 12,
+                },
+                tabBarStyle: {
+                    backgroundColor: COLORS.white,
+                },
+                tabBarActiveTintColor: COLORS.primary,
+                tabBarInactiveTintColor: COLORS.gray,
+            }}>
+            <BottomTab.Screen
+                name="ShopTab"
+                component={ShopNavigator}
+                options={{
+                    tabBarLabel: 'Tienda',
+                    tabBarIcon: ({ focused, color }) => {
+                        return (
+                            <Ionicons
+                                name={focused ? 'home' : 'home-outline'}
+                                size={20}
+                                color={color}
+                            />
+                        );
+                    },
+                }}
+            />
+            <BottomTab.Screen
+                name="CartTab"
+                component={CartNavigator}
+                options={{
+                    tabBarLabel: 'Carrito',
+                    tabBarIcon: ({ focused, color }) => {
+                        return (
+                            <Ionicons
+                                name={focused ? 'cart' : 'cart-outline'}
+                                size={20}
+                                color={color}
+                            />
+                        );
+                    },
+                    tabBarBadge: 2,
+                    tabBarBadgeStyle: {
+                        backgroundColor: COLORS.primary,
+                        color: COLORS.white,
+                        fontFamily: 'Inter-Regular',
+                        fontSize: 11,
+                    },
+                }}
+            />
+            <BottomTab.Screen
+                name="OrdersTab"
+                component={OrdersNavigator}
+                options={{
+                    tabBarLabel: 'Ordenes',
+                    tabBarIcon: ({ focused, color }) => {
+                        return (
+                            <Ionicons
+                                name={focused ? 'file-tray' : 'file-tray-outline'}
+                                size={20}
+                                color={color}
+                            />
+                        );
+                    },
+                }}
+            />
         </BottomTab.Navigator>
     );
 };
--- src/themes/colors/index.js
+++ src/themes/colors/index.js
@@ -5,4 +5,5 @@ export const COLORS = {
     background: '#FEFAE0',
     black: '#000000',
     white: '#FFFFFF',
+    gray: '#CCCCCC',
 };
